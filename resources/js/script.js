cece387a691b600469a6afe02e28bf82bfcab2db
Prince Success Nwuke <prius2055@gmail.com>
2020-08-17 00:49:19 -0700

    my first commit

---
+++ resources/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $('.events-hiking').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+
+      $('.events-os').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+
+      $('.events-picnic').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery:{
+            enabled:true
+        }
+        // other options
+      });
+
+        /*..............NAVIGATING TO DIFFERENT SECTIONS............*/
+$('a[href*="#"]')                                                                    // Select all links with hashes
+.not('[href="#"]')                                                                 // Remove links that don't actually link to anything
+.not('[href="#0"]')
+.click(function(event) {                                                          // On-page links
+  
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {                                                                             // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');     // Does a scroll target exist?
+    if (target.length) {                                                          // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {                                                       // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) {                                               // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1');                                        // Adding tabindex for elements not focusable
+          $target.focus();                                                       // Set focus again
+        };
+      });
+    }
+  }
+});
+    
+  });
